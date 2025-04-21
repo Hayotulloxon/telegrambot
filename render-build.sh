2f846be444bd7fa341fd8290bf1fc391b2391525
Hayotulloxon <143584296+Hayotulloxon@users.noreply.github.com>
2025-04-21 16:03:47 +0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-# FFmpeg-ni o'rnatish
+# FFmpeg va boshqa kerakli paketlarni o'rnatish
 apt-get update -qq && apt-get -y install ffmpeg
+
+# SSL tekshiruvini o'chirish uchun Python muhitini o'zgartirish
+echo "export PYTHONHTTPSVERIFY=0" >> ~/.bashrc
+echo "export PYTHONHTTPSVERIFY=0" >> ~/.profile
+
+# yt-dlp-ni so'nggi versiyaga yangilash
+pip install --upgrade yt-dlp
